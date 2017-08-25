bc2168e7b8d69d1c2e2cda3bccb31fc352113d79
EuGenius\EuGeniusPC <EuGenius.M@ya.ru>
2017-08-25 23:19:55 +0500

    Main and Layout

---
+++ app/src/main/java/com/eugenius/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.eugenius.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by EuGeniusPC on 25.08.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
